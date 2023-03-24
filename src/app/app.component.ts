ba2f67a921dc26e270afe502a4026930a997a1ad
Formateur1 <enseignant.formateur@protonmail.com>
2023-03-24 16:19:17 +0100

    bouton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,8 @@ export class AppComponent {
   titre:string = 'TD1Ex2';
 
   imageUrl:string = "https://www.intiformation.com/wp-content/uploads/2021/05/ezgif-2-2cdc70725914.gif";
+
+  nomClasse:string = "texte";
+
+  bouton:boolean = true;
 }
